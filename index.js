c0c932f508879ca9144e4420e9b45fba03c3fbb4
phithithuhuong <thu872315@gmail.com>
2022-10-09 22:44:19 +0700

    sudungjstronghtml

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//khai bao bien: var
+//var age :khai bao tuoi
+/*
+*comment duoc 1 khoi nhieu dong
+*
+ */
+// alert(fullName);
+// alert(age);
+//  copy ấn ctrl / để comment nhanh hơn
+
+//confirm nằm trong hộp thoại
+//prompt ô input để nhập gì đó vào
+
+// setTimeout(function () {
+//     alert('thong bao cho ban')
+// }, 20000) cho 1 đoạn code chạy sau khoảng 1 tgian
+
+// setInterval chạy 1 đoạn code sau khoang tgian lặp đi lặp lai
+
+// var a = 1 * 2;
+// console.log(a): dạng toán tử số học-arithmetic
+
+// toán tử gán thường được sủ dụng dấu =- assignment
+//
+// var a = 1;
+// var b = 2;   là dạng toán tử so sánh-comparison
+// if (a<b) {
+//     alecrt('dung');
+// }
+
+//var a=1;   toán tủ logic-logical
+//var b=2;
+//if (a > 0 && b>0) {alert(' a&b lớn hơn 0)}
+
+
+
